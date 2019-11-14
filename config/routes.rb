dced2f4480bdb8946f958b1b4e32ca78f394bc3e
Joe Olley <joeolley@hotmail.co.uk>
2019-11-14 17:57:55 +0000

    Cocktail and dose CRUD actions and routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/show'
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/edit'
+  get 'doses/update'
+  get 'doses/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, except: [:new, :create]
+  root to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'starting seed'
+cocktails_string = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+puts 'parsing'
+cocktails_hash = JSON.parse(cocktails_string)
+puts 'returning hash'
+p cocktails_hash
+puts 'iterating'
+cocktails_hash['drinks'].each do |cocktail|
+  Ingredient.create!(name: cocktail['strIngredient1'])
+end
