f681247136c8cf31b8859d6f44c32203060cbfd1
Joe Olley <joeolley@hotmail.co.uk>
2019-11-17 14:24:15 +0000

    review model added with references to cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
   has_one_attached :photo
 end
